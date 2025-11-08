be169d3058e561cbb0fffa4bbec973a0e2077922
mahmud035 <mahamudulhasan4148@gmail.com>
2025-11-08 09:58:58 +0600

    If statement

---
+++ playground/learn-linux-tv/bash-scripts/if-statement.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#* Basic if statement
+
+# myNum=300
+
+# if [ "$myNum" -gt 200 ]; then
+# 	echo "The condition is true."
+# else
+# 	echo "The variable does not equal 200."
+# fi
+
+#* Use if statement for the existence of a file
+
+# if [ -f ~/myfile ]; then
+# 	echo "The file exist."
+# else
+# 	echo "The file does not exist."
+# fi
+
+#* Install a package if it's not already installed
+
+# command=/usr/bin/htop
+
+# if [ -f $command ]; then
+# 	echo "$command is available, let's run it..."
+# else
+# 	echo "$command is NOT available, installing it..."
+# 	sudo apt update && sudo apt install -y htop
+# fi
+
+# $command
+
---
+++ playground/learn-linux-tv/bash-scripts/myscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="Mahamudul Hasan"
+now=$(date)
+
+echo "Hello $name"
+echo "The system time and date is:"
+echo $now
+echo "Your username is: $USER"
